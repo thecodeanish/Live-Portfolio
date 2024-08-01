d052d0da19ad8b8b926387199347ea271c7206b7
thecodeanish <thecode.anish@gmail.com>
2024-08-01 13:50:12 +0530

    Some changes in HTML file, CSS, and JavaScript File

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -15,3 +15,42 @@ document.addEventListener("DOMContentLoaded", function () {
   typing.start();
 });
 /* ===> Script for Home auto typing section End <=== */
+
+/* ===> Script for Sidebar Start <=== */
+const sidebar = document.querySelector(".sidebar");
+const navList = sidebar.querySelectorAll("li");
+const totalNavList = navList.length;
+const allSection = document.querySelectorAll(".section"),
+  totalSection = allSection.length;
+
+for (let i = 0; i < totalNavList; i++) {
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    for (let i = 0; i < totalSection; i++) {
+      allSection[i].classList.remove("back-section");
+    }
+    for (let j = 0; j < totalNavList; j++) {
+      if (navList[j].querySelector("a").classList.contains("active")) {
+        allSection[j].classList.add("back-section");
+      }
+      navList[j].querySelector("a").classList.remove("active");
+    }
+    this.classList.add("active");
+    showSection(this);
+  });
+}
+function showSection(element) {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("active");
+  }
+  const target = element.getAttribute("href").split("#")[1];
+  document.querySelector("#" + target).classList.add("active");
+}
+// Script for Nav Toggler
+const navTogglerBtn = document.querySelector(".nav-toggler");
+navTogglerBtn.addEventListener("clicl", () => {
+  sidebarTogglerSectionBtn();
+});
+function sidebarTogglerSectionBtn() {
+  sidebar.classList.toggle("open");
+}
